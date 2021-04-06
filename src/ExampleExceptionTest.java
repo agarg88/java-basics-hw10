2789d2665aa07b8efff3ba69aa51d9169c982011
anshugar <anshugar@amazon.com>
2021-04-05 18:18:12 -0700

    Submitting java-basics-hw10 homework

--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -1,5 +1,4 @@
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.fail;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -15,8 +14,10 @@ public class ExampleExceptionTest {
   public static Object[][] data() {
     return new Object[][]{
         {2, 2, 4},
-        {2, 3, 6}
+        {2, 3, 6},
         // TODO add 2 more test data here
+        {12, 0, 0},
+        {99, 77, 7623}
     };
   }
 
@@ -29,19 +30,26 @@ public class ExampleExceptionTest {
   public static Object[][] negativeData() {
     return new Object[][]{
         {-2, 2},
-        {2, -2}
+        {2, -2},
         // TODO add 2 more test data here
+        {-98, -89},
+        {0, -6}
     };
   }
 
   @Test(dataProvider = "data")
   public void testRectangleArea(int a, int b, int c) {
     // TODO put your code here
+    assertEquals(ExampleException.rectangleArea(a,b),c, "Rectangle area is not correct");
   }
 
 
-  @Test(dataProvider = "negativeData")
+  @Test(dataProvider = "negativeData", expectedExceptions = IllegalArgumentException.class,
+          expectedExceptionsMessageRegExp = "input value is below zero!")
   public void testRectangleAreaNegative(int a, int b) {
     // TODO put your code here
+    int area = ExampleException.rectangleArea(a,b);
+    assertEquals(ExampleException.rectangleArea(a,b),area, "Rectangle area is not correct");
+
   }
 }
